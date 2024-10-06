179b0f49aa926e026fde301d533e7178ba65e798
Paulus sesa <114337187+seanpaulsesa@users.noreply.github.com>
2024-10-06 11:18:53 -0700

    feat: menambahkan file file php

---
+++ tipedata.number.php
@@ -0,0 +1,30 @@
+<?php
+echo "Decimal :";
+var_dump(1234);
+
+echo "Octal :";
+var_dump(01234);
+
+echo "Hexadicimal :";
+var_dump(0x1a);
+
+echo "Binary :";
+var_dump(0b111111);
+
+echo "Underscore in number :";
+var_dump(1_241_241_241);
+
+echo 'floating point';
+var_dump('1.234');
+
+echo 'floating point dengan E notation : (1.7 x 1000) : ';
+var_dump('1.7e3');
+
+echo 'floating point dengan E notation minus : (1.7 x 0.001) : ';
+var_dump('1.7e-3');
+
+echo 'underscore di floating point :';
+var_dump('1_123.123');
+
+echo 'integer overflow :';
+var_dump('9223372036854775808');
---
+++ tipedatastring.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'Nama : ';
+echo 'Paulus Sesa : ';
+echo "\n";
+
+echo "Nama : ";
+echo "Paulus Sesa ";
